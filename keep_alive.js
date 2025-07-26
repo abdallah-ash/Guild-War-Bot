82febd3362b7f1fc6f5e13140a902474ac81f89b
Abdallah <abdallahashour858@gmail.com>
2025-07-26 19:21:30 +0300

    Changed require to import in keep_alive.js

--- keep_alive.js
+++ keep_alive.js
@@ -1,5 +1,7 @@
-// keepAlive.js
-const express = require("express");
+import express from "express";
+
 const app = express();
+
 app.get("/", (req, res) => res.send("Bot is alive!"));
+
 app.listen(3000, () => console.log("KeepAlive server running"));
