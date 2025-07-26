8cc8d2b09105a98f3aee7941b53f25d754aeec97
Abdallah <abdallahashour858@gmail.com>
2025-07-26 18:33:57 +0300

    Bot testing phase

---
+++ index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+import { Client, GatewayIntentBits } from 'discord.js'
+import cron from 'node-cron'
+
+const client = new Client({
+    intents: [GatewayIntentBits.Guilds],
+});
+
+client.once('ready', () => {
+    console.log(`Logged in as ${client.user.tag}`)
+
+    cron.schedule('* * * * *', async () => {
+        const channel = await client.channels.fetch('1378026320155381820');
+        channel.send("<@&1378006278877614211> balls");
+    }, {
+        timezone: "Africa/Cairo"
+    });
+});
+
+client.login(process.env.DISCORD_TOKEN);
